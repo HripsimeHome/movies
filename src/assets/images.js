b9d5a2f0cced26465ff09a57668300131295c4b7
Hripsime <m_hripsime@mail.ru>
2025-08-20 14:11:26 +0400

    Addeed trendig omages

--- src/assets/images.js
+++ src/assets/images.js
@@ -3,6 +3,31 @@ import coverImage from "./images/home/coverImage.png";
 import coversWebpImage from "./images/home/coverImage.webp";
 import titleImage from "./images/home/title.png";
 
+// Home, Trending
+import trending1Image from "./images/home/trending/trending1.png";
+import trending1WebpImage from "./images/home/trending/trending1.webp";
+
+import trending2Image from "./images/home/trending/trending2.png";
+import trending2WebpImage from "./images/home/trending/trending2.webp";
+
+import trending3Image from "./images/home/trending/trending3.png";
+import trending3WebpImage from "./images/home/trending/trending3.webp";
+
+import trending4Image from "./images/home/trending/trending4.png";
+import trending4WebpImage from "./images/home/trending/trending4.webp";
+
+import trending5Image from "./images/home/trending/trending5.png";
+import trending5WebpImage from "./images/home/trending/trending5.webp";
+
+import trending6Image from "./images/home/trending/trending6.png";
+import trending6WebpImage from "./images/home/trending/trending6.webp";
+
+import trending7Image from "./images/home/trending/trending7.png";
+import trending7WebpImage from "./images/home/trending/trending7.webp";
+
+import trending8Image from "./images/home/trending/trending8.png";
+import trending8WebpImage from "./images/home/trending/trending8.webp";
+
 //Sidebar
 import avatarImage from "./images/sidebar/avatar.jpg";
 
@@ -11,6 +36,24 @@ export {
   coverImage,
   coversWebpImage,
   titleImage,
+
+  // Home, Trending
+  trending1Image,
+  trending1WebpImage,
+  trending2Image,
+  trending2WebpImage,
+  trending3Image,
+  trending3WebpImage,
+  trending4Image,
+  trending4WebpImage,
+  trending5Image,
+  trending5WebpImage,
+  trending6Image,
+  trending6WebpImage,
+  trending7Image,
+  trending7WebpImage,
+  trending8Image,
+  trending8WebpImage,
 
   //Sidebar
   avatarImage,
--- src/components/HomePage/HomeMain/HomeTrending/HomeTrending.jsx
+++ src/components/HomePage/HomeMain/HomeTrending/HomeTrending.jsx
@@ -1,11 +1,42 @@
 import styles from "./HomeTrending.module.scss";
-// import ImageWebp from "../../layout/ImageWebp/ImageWebp";
-// import { JacobWebpImage } from "../../../assets/images";
+import ImageWebp from "../../../layout/ImageWebp/ImageWebp";
+import {
+  trending1Image,
+  trending1WebpImage,
+  trending2Image,
+  trending2WebpImage,
+  trending3Image,
+  trending3WebpImage,
+  trending4Image,
+  trending4WebpImage,
+  trending5Image,
+  trending5WebpImage,
+  trending6Image,
+  trending6WebpImage,
+  trending7Image,
+  trending7WebpImage,
+  trending8Image,
+  trending8WebpImage,
+} from "../../../../assets/images";
 
 const HomeTrending = () => {
   return (
     <section className={styles.homeTrending}>
-      <div className={styles.homeTrending__title}>Trending New</div>
+      <h3 className={styles.homeTrending__title}>Trending New</h3>
+      <div className={styles.homeTrending__sliderContainer}>
+        <ImageWebp
+          src={trending1Image}
+          alt=""
+          srcSet={trending1WebpImage}
+          className={styles.homeTrending__img}
+        />
+        <ImageWebp
+          src={trending2Image}
+          alt=""
+          srcSet={trending2WebpImage}
+          className={styles.homeTrending__img}
+        />
+      </div>
     </section>
   );
 };
